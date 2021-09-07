406f824233e654ab127ad244ae8f97dfb7f7b364
Oliver Schonrock <oliver@schonrocks.com>
2021-09-07 11:07:06 +0100

    reduce lint warnings and ignore leaderboard data

--- apps/qanda.cpp
+++ apps/qanda.cpp
@@ -1,18 +1,21 @@
 #include "qa_set.hpp"
 #include <iostream>
+#include <string>
 
-int main(int argc, char** argv) {
-  if (argc < 2) {
-    std::cerr << "Usage: " << argv[0] << " question_answer.csv\n"; // NOLINT
+int main(int argc, char* argv[]) {
+  std::vector<std::string> args(argv, argv + argc);
+  
+  if (args.size() < 2) {
+    std::cerr << "Usage: " << args[0] << " question_answer.csv\n";
     return EXIT_FAILURE;
   }
   qa_set qa_set;
-  if (!qa_set.open(argv[1])) {  // NOLINT
-    std::cerr << "Couldn't open: '" << argv[1] << "'\n";  // NOLINT
+  if (!qa_set.open(args[1])) {
+    std::cerr << "Couldn't open: '" << args[1] << "'\n";
     return EXIT_FAILURE;
   }
   if (!qa_set.load()) {
-    std::cerr << "No questions and answers found in : '" << argv[1] << "'\n";  // NOLINT
+    std::cerr << "No questions and answers found in : '" << args[1] << "'\n";
     return EXIT_FAILURE;
   }
   qa_set.run();
--- apps/visitor.cpp
+++ apps/visitor.cpp
@@ -1,13 +1,14 @@
 #include <algorithm>
 #include <benchmark/benchmark.h>
+#include <cstddef>
 #include <random>
 #include <unordered_set>
 #include <vector>
 
 void gen1(benchmark::State& state) {
-  std::size_t                                no_nodes = state.range(0);
+  auto                                       no_nodes = static_cast<std::size_t>(state.range(0));
   std::size_t                                no_edges = no_nodes * 2;
-  std::mt19937_64                            rgen(1);
+  std::mt19937_64                            rgen(1); // NOLINT fixed seed
   std::uniform_int_distribution<std::size_t> dist(0, no_nodes - 1);
 
   std::vector<std::size_t> rvs(no_edges);
@@ -15,6 +16,7 @@ void gen1(benchmark::State& state) {
 
   for (auto _: state) {
     std::vector<std::size_t> uniqverts;
+    uniqverts.reserve(rvs.size());
     for (auto&& e: rvs) uniqverts.push_back(e);
     std::sort(begin(uniqverts), end(uniqverts));
     uniqverts.erase(std::unique(begin(uniqverts), end(uniqverts)), uniqverts.end());
@@ -26,12 +28,12 @@ void gen1(benchmark::State& state) {
     benchmark::DoNotOptimize(max);
   }
 }
-BENCHMARK(gen1)->Range(8, 8 << 16);
+BENCHMARK(gen1)->Range(8, 8U << 16U);
 
 void gen2(benchmark::State& state) {
-  std::size_t                                no_nodes = state.range(0);
+  auto                                       no_nodes = static_cast<std::size_t>(state.range(0));
   std::size_t                                no_edges = no_nodes * 2;
-  std::mt19937_64                            rgen(1);
+  std::mt19937_64                            rgen(1); // NOLINT fixed seed
   std::uniform_int_distribution<std::size_t> dist(0, no_nodes - 1);
 
   std::vector<std::size_t> rvs(no_edges);
@@ -49,6 +51,6 @@ void gen2(benchmark::State& state) {
     benchmark::DoNotOptimize(max);
   }
 }
-BENCHMARK(gen2)->Range(8, 8 << 16);
+BENCHMARK(gen2)->Range(8, 8U << 16U);
 
 BENCHMARK_MAIN();
