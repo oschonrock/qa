f17c16a76ba5fc1ec11561d9e1be70c77380bda7
Oliver Schönrock <oliver@openbrackets.net>
2021-10-07 17:30:21 +0100

    after many perf experiments.. this feels good

--- apps/hibp.cpp
+++ apps/hibp.cpp
@@ -1,43 +1,42 @@
 #include "os/algo.hpp"
 #include "os/bch.hpp"
-#include "os/fs.hpp"
 #include "os/str.hpp"
 #include "sha1/sha1.hpp"
-#include <algorithm>
 #include <array>
 #include <compare>
+#include <cstddef>
 #include <cstdint>
-#include <cstdio>
 #include <exception>
 #include <filesystem>
 #include <fstream>
 #include <iomanip>
+#include <ios>
 #include <iostream>
 #include <optional>
 #include <stdexcept>
+#include <streambuf>
 #include <string>
 
+namespace hibp {
+// unsigned because the next ops are likely bit-wise
 unsigned make_nibble(char nibblechr) {
   int nibble = nibblechr - '0';
-  if (nibble > 9) nibble = nibble - ('A' - '0') + 10;
+  if (nibble > 9) nibble = (nibble & ~('a' - 'A')) - ('A' - '0') + 10; // NOLINT
+  assert(nibble >= 0 and nibble <= 15);                                // NOLINT decay
   return static_cast<unsigned>(nibble);
 }
 
 struct password {
+  password() = default;
+
   password(std::ifstream& db, std::size_t pos) { // NOLINT initialization
-    // the below is about 1.5x - 2x faster (but requires non-standard `cfile` wrapper)
-    // std::FILE* db_cfile = os::fs::cfile(db);
-    // std::fseek(db_cfile, static_cast<long>(pos * sizeof(password)), SEEK_SET);
-    // std::fread(this, sizeof(*this), 1, db_cfile);
     db.seekg(static_cast<long>(pos * sizeof(password)));
     db.read(reinterpret_cast<char*>(this), sizeof(*this)); // NOLINT reinterpret_cast
   }
 
   // line must be an upppercase sha1 hexstr with optional ":123" appended (123 is the count).
-  explicit password(const std::string& line) { // NOLINT initlialisation
-    assert(hexstr.length() >= hash.size() * 2);
-    // do this here in constr body and not in constr init list with functions call, because of this:
-    // https://stackoverflow.com/questions/69427517/nrvo-of-stdarray-in-constructor-initialiser-list
+  explicit password(const std::string& line) {   // NOLINT initlialisation
+    assert(line.length() >= hash.size() * 2);    // NOLINT decay
     for (auto [i, b]: os::algo::enumerate(hash)) // note b is by reference!
       b = static_cast<std::byte>(make_nibble(line[2 * i]) << 4U | make_nibble(line[2 * i + 1]));
 
@@ -62,65 +61,104 @@ struct password {
   std::array<std::byte, 20> hash;
   int32_t                   count; // be definitive about size
 };
+ 
+void build(std::istream& text_stream, std::ostream& binary_stream) {
+  // convert text file to binary. left here for future updates
+  // std::getline is about 2x faster than `is >> line` here
 
-std::optional<password> search(password needle, std::ifstream& db, std::size_t db_size) {
-  std::size_t count = db_size;
-  std::size_t first = 0;
-  {
-    os::bch::Timer t("search");
+  // Using std::fwrite is about 8x faster than std::ostream::write.
+  // But only if writing 1 password at a time. Instead buffer 100 of them.
+  constexpr std::size_t         obufcnt = 100;
+  std::array<password, obufcnt> obuf{};
+  std::size_t                   obufpos = 0;
+  for (std::string line; std::getline(text_stream, line);) {
+
+    password pw(line);
+    if (obufpos == obuf.size()) {
+      binary_stream.write(reinterpret_cast<char*>(&obuf), // NOLINT reincast
+                          static_cast<std::streamsize>(sizeof(password) * obuf.size()));
+      obufpos = 0;
+    }
+    std::memcpy(&obuf[obufpos], &pw, sizeof(pw));
+    ++obufpos;
+  }
+  if (obufpos > 0)
+    binary_stream.write(reinterpret_cast<char*>(&obuf), // NOLINT reincast
+                        static_cast<std::streamsize>(sizeof(password) * obufpos));
+}
+
+class database {
+public:
+  explicit database(std::string dbfilename)
+      : dbfilename_(std::move(dbfilename)), dbpath_(dbfilename_),
+        dbfsize_(std::filesystem::file_size(dbpath_)), db_(dbpath_, std::ios::binary) {
+
+    if (dbfsize_ % sizeof(password) != 0)
+      throw std::domain_error("db file size is not a multiple of the record size");
+
+    dbsize_ = dbfsize_ / sizeof(password);
+
+    if (!db_.is_open()) throw std::domain_error("cannot open db: " + std::string(dbpath_));
+  }
+
+  std::optional<password> search(password needle) {
+    std::size_t count = dbsize_;
+    std::size_t first = 0;
     // lower_bound binary search algo
     while (count > 0) {
       std::size_t pos  = first;
       std::size_t step = count / 2;
       pos += step;
-      password cur(db, pos);
+      password cur(db_, pos);
       if (cur < needle) { // NOLINT weird nullptr warning
         first = ++pos;
         count -= step + 1;
       } else
         count = step;
     }
-    if (first < db_size) {
-      password found(db, first);
+    if (first < dbsize_) {
+      password found(db_, first);
       if (found == needle) return found;
     }
+    return std::nullopt;
   }
-  return std::nullopt;
-}
+
+  std::optional<password> search(const std::string& sha1_pw_hash_txt) {
+    hibp::password needle(sha1_pw_hash_txt);
+    return search(needle);
+  }
+
+  std::ifstream& db() { return db_; }
+
+private:
+  std::string           dbfilename_;
+  std::filesystem::path dbpath_;
+  std::size_t           dbfsize_;
+  std::size_t           dbsize_;
+  std::ifstream         db_;
+};
+
+} // namespace hibp
 
 int main(int argc, char* argv[]) {
   std::ios_base::sync_with_stdio(false);
 
-  // // convert text file to binary. left here for future updates
-  // // std::getline is about 2x faster than `std::cin >> line` here
-  // for (std::string line; std::getline(std::cin, line);) {
-  //   password pw(line);
-  //   // this is about 8x faster than std::cout.write(reinterpret_cast<char*>(&pw), sizeof(pw))
-  //   std::fwrite(&pw, sizeof(pw), 1, stdout);
-  // }
-  // return 0;
+  // build db
+  hibp::build(std::cin, std::cout);
+  return 0;
 
   try {
     if (argc < 3)
       throw std::domain_error("USAGE: " + std::string(argv[0]) + " dbfile.bin plaintext_password");
 
-    auto dbpath = std::filesystem::path(argv[1]);
-
-    std::size_t db_file_size = std::filesystem::file_size(dbpath);
-    if (db_file_size % sizeof(password) != 0)
-      throw std::domain_error("db_file_size is not a multiple of the record size");
-    std::size_t db_size = db_file_size / sizeof(password);
-
-    auto db = std::ifstream(dbpath, std::ios::binary);
-    if (!db.is_open())
-      throw std::domain_error("cannot open db: " + std::string(dbpath));
+    hibp::database db(argv[1]);
 
     SHA1 sha1;
     sha1.update(argv[2]);
-    password needle(sha1.final());
+    hibp::password needle(sha1.final());
 
     std::cout << "needle = " << needle << "\n";
-    auto found = search(needle, db, db_size);
+    auto found = db.search(needle);
 
     if (found)
       std::cout << "found  = " << *found << "\n";
